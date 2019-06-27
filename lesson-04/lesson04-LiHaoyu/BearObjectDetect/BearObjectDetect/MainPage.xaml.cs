81f583d3a27eb112b40997a3c1ea99069939243a
Yanyueyimeng <lihy@inesa-lab.com>
2019-06-27 16:48:49 +0800

    success

---
+++ lesson-04/lesson04-LiHaoyu/BearObjectDetect/BearObjectDetect/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using CustomVision;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace BearObjectDetect
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TbRun_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            tbBearType.Text = string.Empty;
+
+            Uri imageUri = null;
+            try
+            {
+                imageUri = new Uri(tbImageUrl.Text);
+            }
+            catch (Exception)
+            {
+                tbBearType.Text = "URL不合法";
+                return;
+            }
+
+            tbBearType.Text = "加载图片...";
+            imgBear.Source = new BitmapImage(imageUri);
+
+        }
+
+        private void ImgBear_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            RecognizeBear();
+        }
+
+        private void ImgBear_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            tbBearType.Text = "图片加载失败";
+        }
+
+
+        private async void RecognizeBear()
+        {
+            // 加载模型
+            RenderTargetBitmap rtb = new RenderTargetBitmap();
+            await rtb.RenderAsync(imgBear);
+
+            // 取得所有像素值
+            var pixelBuffer = await rtb.GetPixelsAsync();
+
+            // 构造模型需要的输入格式
+            SoftwareBitmap softwareBitmap = SoftwareBitmap.CreateCopyFromBuffer(pixelBuffer, BitmapPixelFormat.Bgra8, rtb.PixelWidth, rtb.PixelHeight);
+            VideoFrame videoFrame = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+
+            //Create writealbe Bitmap
+            WriteableBitmap writeableBmp = new WriteableBitmap(softwareBitmap.PixelWidth, softwareBitmap.PixelHeight);
+            softwareBitmap.CopyToBuffer(writeableBmp.PixelBuffer);
+
+            //导入模型
+            StorageFile modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/model.onnx"));
+            List<string> labels = new List<string>();
+            labels.Add("bear");
+            labels.Add("pig");
+            ObjectDetection model = new ObjectDetection(labels);
+            await model.Init(modelFile);
+
+            // 推理
+            //BearModelOutput output = await model.EvaluateAsync(bearModelInput);
+            List<PredictionModel> listPredictionModel;
+            listPredictionModel = await model.PredictImageAsync(videoFrame) as List<PredictionModel>;
+
+            PredictionModel modelResult = null;
+            float probability = 0;
+            foreach (PredictionModel pItem in listPredictionModel)
+            {
+
+                if (pItem.Probability > probability)
+                {
+                    modelResult = pItem;
+                    probability = pItem.Probability;
+                }
+
+            }
+            // writeableBmp.DrawRectangle(254, 39, 275, 242, Colors.Red);
+            if (probability > 0)
+            {
+                int left, top, width, height;
+                left = (int)(modelResult.BoundingBox.Left * writeableBmp.PixelWidth);
+                top = (int)(modelResult.BoundingBox.Top * writeableBmp.PixelHeight);
+                width = (int)(modelResult.BoundingBox.Width * writeableBmp.PixelWidth);
+                height = (int)(modelResult.BoundingBox.Height * writeableBmp.PixelHeight);
+
+                writeableBmp.DrawRectangle(left, top, left + width, top + height, Colors.Red);
+                tbBearType.Text = modelResult.TagName;
+                //writeableBmp.DrawRectangle(111, 189, (int) (232*1.4), (int) (299*1.4), Colors.Red);
+                imgBear.Source = writeableBmp;
+            }
+
+        }
+    }
+}
---
+++ lesson-04/lesson04-LiHaoyu/LightControl/source/LightControl/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CognitiveServices.Speech;
+using Newtonsoft.Json;
+
+namespace LightControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "开始";
+            pictureBox1.Load("LightOff.png");
+        }
+
+        // 设置服务信息
+
+        // 设置语音服务密钥及区域
+        // 可在 https://azure.microsoft.com/zh-cn/try/cognitive-services/my-apis/?api=speech-services 中找到
+        // 密钥示例: 5ee7ba6869f44321a40751967accf7b9
+        // 区域示例: westus
+        const string speechKey = "0183639b4acd4620a51fdd53383297e3";
+        const string speechRegion = "westus";
+
+        // 设置语言理解服务地址
+        // 示例: https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/102f6255-0c32-4f46-9c79-fe12fea4d6c4?subscription-key=8004421650254a74875cf3c888b1d11f&verbose=true&timezoneOffset=0&q=
+        // 可在 https://www.luis.ai 中进入app右上角publish中找到
+        const string luisEndpoint = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/15f8a1f0-5952-4c85-9a53-9acd9779a0fb?verbose=true&timezoneOffset=-360&subscription-key=65dc9d18330a4159b947b1c02ca793b9&q=";
+
+        //// 语音识别器
+        SpeechRecognizer recognizer;
+        bool isRecording = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        // 获得音频分析后的文本内容
+        private void Recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
+        {
+            if (e.Result.Reason == ResultReason.RecognizedSpeech)
+            {
+                Log("最终结果: " + e.Result.Text);
+                ProcessSttResult(e.Result.Text);
+            }
+        }
+
+        // 识别过程中的中间结果
+        private void Recognizer_Recognizing(object sender, SpeechRecognitionEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Result.Text))
+            {
+                Log("中间结果: " + e.Result.Text);
+            }
+        }
+
+        private async void ProcessSttResult(string text)
+        {
+            // 调用语言理解服务取得用户意图
+            string intent = await GetLuisResult(text);
+
+            // 按照意图控制灯
+            if (!string.IsNullOrEmpty(intent))
+            {
+                if (intent.Equals("KeTingDingDengOn", StringComparison.OrdinalIgnoreCase))
+                {
+                    OpenKeTingDingDeng();
+                }
+                else if (intent.Equals("KeTingDingDengOff", StringComparison.OrdinalIgnoreCase))
+                {
+                    CloseKeTingDingDeng();
+                }
+                else if (intent.Equals("WoShiDingDengOn", StringComparison.OrdinalIgnoreCase))
+                {
+                    OpenWoShiDingDeng();
+                }
+                else if (intent.Equals("WoShiDingDengOff", StringComparison.OrdinalIgnoreCase))
+                {
+                    CloseWoShiDingDeng();
+                }
+                else if (intent.Equals("WoShiTaiDengOff", StringComparison.OrdinalIgnoreCase))
+                {
+                    OpenWoShiTaiDeng();
+                }
+                else if (intent.Equals("WoShiTaiDengOff", StringComparison.OrdinalIgnoreCase))
+                {
+                    CloseWoShiTaiDeng();
+                }
+            }
+        }
+
+        private async Task<string> GetLuisResult(string text)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string luisJson = await httpClient.GetStringAsync(luisEndpoint + text);
+
+                try
+                {
+                    dynamic result = JsonConvert.DeserializeObject<dynamic>(luisJson);
+                    if (result.topScoringIntent == null)
+                    {
+                        return null;
+                    }
+                    string intent = (string)result.topScoringIntent.intent;
+                    double score = (double)result.topScoringIntent.score;
+                    Log("意图: " + intent + "\r\n得分: " + score + "\r\n");
+
+                    return intent;
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.ToString());
+                    return null;
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            isRecording = !isRecording;
+            if (isRecording)
+            {
+                // 启动识别器
+                await Initialize();
+                button1.Text = "停止";
+            }
+            else
+            {
+                // 停止识别器
+                await UnloadRecognizer();
+                button1.Text = "开始";
+            }
+
+            button1.Enabled = true;
+        }
+
+        private async Task Initialize()
+        {
+            try
+            {
+                var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+                speechConfig.SpeechRecognitionLanguage = "zh-CN";
+
+                // 设置识别中文
+                recognizer = new SpeechRecognizer(speechConfig);
+
+                // 挂载识别中的事件
+
+                // 收到中间结果
+                recognizer.Recognizing += Recognizer_Recognizing;
+                // 收到最终结果
+                recognizer.Recognized += Recognizer_Recognized;
+
+                await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(true);
+            }
+            catch (Exception ex)
+            {
+                if (ex is System.TypeInitializationException)
+                {
+                    Log("类型初始化时遇到异常：{0}\r\n请确认项目在x64模式下编译，而不是Any CPU。", ex.ToString());
+                }
+                else
+                {
+                    Log("异常：{0}\r\n初始化出错，请确认麦克风正常。", ex.ToString());
+                    Log("已降级到文本语言理解模式");
+
+                    TextBox inputBox = new TextBox();
+                    inputBox.Text = "";
+                    inputBox.Size = new Size(300, 26);
+                    inputBox.Location = new Point(10, 10);
+                    inputBox.KeyDown += inputBox_KeyDown;
+                    Controls.Add(inputBox);
+
+                    button1.Visible = false;
+                }
+            }
+
+        }
+
+        private async Task UnloadRecognizer()
+        {
+            await recognizer.StopContinuousRecognitionAsync();
+            // 收到中间结果
+            recognizer.Recognizing -= Recognizer_Recognizing;
+            // 收到最终结果
+            recognizer.Recognized -= Recognizer_Recognized;
+            recognizer.Dispose();
+            recognizer = null;
+        }
+
+        #region 界面操作
+
+        private void Log(string message, params string[] parameters)
+        {
+            MakesureRunInUI(() =>
+            {
+                if (parameters != null && parameters.Length > 0)
+                {
+                    message = string.Format(message + "\r\n", parameters);
+                }
+                else
+                {
+                    message += "\r\n";
+                }
+                textBox1.AppendText(message);
+            });
+        }
+
+        private void OpenKeTingDingDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("KeTingDingDengOn.png");
+            });
+        }
+
+        private void OpenWoShiDingDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("WoShiDingDengOn.png");
+            });
+        }
+
+        private void OpenWoShiTaiDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("WoShiTaiDengOn.png");
+            });
+        }
+
+        private void CloseKeTingDingDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("KeTingDingDengOff.png");
+            });
+        }
+
+        private void CloseWoShiDingDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("WoShiDingDengOff.png");
+            });
+        }
+
+        private void CloseWoShiTaiDeng()
+        {
+            MakesureRunInUI(() =>
+            {
+                pictureBox1.Load("WoShiTaiDengOff.png");
+            });
+        }
+
+        private void MakesureRunInUI(Action action)
+        {
+            if (InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(action);
+                Invoke(action, null);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void inputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && sender is TextBox)
+            {
+                TextBox textBox = sender as TextBox;
+                e.Handled = true;
+                Log(textBox.Text);
+                ProcessSttResult(textBox.Text);
+                textBox.Text = string.Empty;
+            }
+        }
+
+        #endregion
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
