779243650881ee8af93f54ded11e1d1b564f73c0
wdy0401 <wdy0401@gmail.com>
2019-07-04 17:01:26 +0800

    王德扬-lesson05

--- lesson-05/lesson05-HW-wangdeyang.py
+++ lesson-05/lesson05-HW-wangdeyang.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
