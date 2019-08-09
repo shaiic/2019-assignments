749bd79f5a3a77a3af4733d6ca34af6d3082d10d
hnzkliuwei <40585098+hnzkliuwei@users.noreply.github.com>
2019-08-09 08:11:19 +0000

    刘伟-lesson08课作业提交 (#165)
    * commit lesson03 liuwei
    
    * lesson02 liuwei
    
    * pull quest test
    
    * liuwei lession04 light commit
    
    * lesson05 刘伟的作业
    
    * 删除C#工程代码，太大了
    
    * lesson06 刘伟的作业
    
    * lesson-08作业提交

---
+++ lesson-08/lesson08-HW-LiuWei/openpai-mnist/run_samples.py
@@ -0,0 +1,27 @@
+import os
+import hdfshelper
+import subprocess
+
+print("~~~~~~PAI_JOB_NAME: ", os.environ['PAI_JOB_NAME'])
+#jobName = os.environ['PAI_JOB_NAME'].split('~')[1]\
+jobName = os.environ['PAI_JOB_NAME']
+root_dir = "/resource/"
+
+print("Downloading data...")
+hdfshelper.Download(os.path.join(root_dir, "data"), ".")
+hdfshelper.Download(os.path.join(root_dir, "code", "pip_requirements_gpu.txt"), ".")
+# hdfshelper.Download(root_dir + "data", ".")
+# hdfshelper.Download(root_dir + "code/" + "pip_requirements_gpu.txt", ".")
+
+print("Running...")
+subprocess.call("./train.sh", shell=True)
+
+print("Uploading data...")
+output_dir = os.path.join(root_dir, "output", jobName)
+hdfshelper.Upload("./output/", output_dir)
+# hdfshelper.Upload("./data/checkpoint", output_dir)
+# hdfshelper.Upload("./data/model.ckpt-200000.data-00000-of-00003", output_dir)
+# hdfshelper.Upload("./data/model.ckpt-200000.data-00001-of-00003", output_dir)
+# hdfshelper.Upload("./data/model.ckpt-200000.data-00002-of-00003", output_dir)
+# hdfshelper.Upload("./data/model.ckpt-200000.index", output_dir)
+# hdfshelper.Upload("./data/model.ckpt-200000.meta", output_dir)
